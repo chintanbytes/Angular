317396eee7427644f01fb5aff2c37e03de4a5db1
Chintan Joshi <chintan.joshi@hubparking.com>
2023-05-09 19:31:39 -0400

    Fixing warning for non-null ref

--- WebApi/Reporitories/RepositoryResult.cs
+++ WebApi/Reporitories/RepositoryResult.cs
@@ -2,22 +2,22 @@ namespace MyShop.WebApi.Repositories;
 public class Result<T>
 {
     public bool Success { get; }
-    public T Data { get; }
-    public string ErrorMessage { get; }
+    public T? Data { get; }
+    public string? ErrorMessage { get; }
 
-    private Result(bool success, T data, string errorMessage)
+    private Result(bool success, T? data, string? errorMessage)
     {
         Success = success;
         Data = data;
         ErrorMessage = errorMessage;
     }
 
-    public static Result<T> SetSuccess(T data)
+    public static Result<T> SetSuccess(T? data)
     {
         return new Result<T>(true, data, default(string));
     }
 
-    public static Result<T> SetFailure(string errorMessage)
+    public static Result<T> SetFailure(string? errorMessage)
     {
         return new Result<T>(false, default(T), errorMessage);
     }
